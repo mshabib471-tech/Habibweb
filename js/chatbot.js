ab96f13a9eee8ee18f1ff941c670feed63134a4a
Habib-YT <mshabib471@gmail.com>
2026-09-15 20:46:03 +0600

    Update Google Apps Script URL in chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -105,7 +105,7 @@ async function sendHabibManualMessage() {
     msgBox.innerHTML += `<div id="${loadId}" class="bg-slate-800 p-3 rounded-2xl max-w-[85%] text-slate-400 italic">Sending...</div>`;
     msgBox.scrollTop = msgBox.scrollHeight;
 
-    const scriptURL = "https://script.google.com/macros/s/AKfycbwfuDR9Tiopev24MWY964k4ZTF6vIaVyzus0_-ueH-6/exec";
+    const scriptURL = "https://script.google.com/macros/s/AKfycbym-OMBAUd5MJVj-S3p0W6CwoDbLsT5PObpkD4YEbeQeeLagv3QHFEsinI2HUt-XlhBOg/exec";
     
     try {
         await fetch(scriptURL, {
